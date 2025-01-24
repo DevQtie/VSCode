79572fb53bd7b26b5b6552438c130096ab44bbf1
DevQtie <rpi.alaguartilla@gmail.com>
2025-01-24 12:39:35 +0800

    Node.JS API v1.10.2 with service.js Dynamic Name and Path Script

--- service.js
+++ service.js
@@ -1,4 +1,14 @@
 import { Service } from 'node-windows';
+import { createRequire } from 'module';
+
+
+const require = createRequire(import.meta.url);
+const packageJson = require('./package.json');
+
+const appVersion = packageJson.version;
+
+/*
+import { Service } from 'node-windows';
 
 const svc = new Service({
     name: 'apsalesmobileapi',
@@ -7,6 +17,18 @@ const svc = new Service({
 });
 svc.on('install', () => svc.start());
 svc.install();
+*/ // uncomment to test (install) it locally on your device
+
+/* NO NEED TO MODIFY THIS AS LONG AS YOU RENAME YOUR API'S PARENT FOLDER WITH THIS INSTANCE PATTERN */
+
+const svc = new Service({
+    name: `apsalesmobileapi_v${appVersion}`,
+    description: 'Node application as Windows Service',
+    script: `C:\\Users\\Administrator\\API\\VSCode_v${appVersion}\\VSCode\\server.js`
+});
+svc.on('install', () => svc.start());
+svc.install(); // to install on server side device
+
 
 /*-- To run this service.js --*/
 
